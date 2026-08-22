9a4cbf5732cbd30db45414040e32a87f20af80b6
ABHINANDAN VERMA <abhinandanverma0152@gmail.com>
2026-08-23 03:07:58 +0530

    initial file

---
+++ Lecture29_Jsfoundation/index.js
@@ -0,0 +1,55 @@
+// console.log("Hi India");
+// console.log(5+4);
+// console.log("5*4=",4*5);
+// age=23;
+// console.log(age);
+
+// name="abhi";
+// console.log(name);
+// console.log(age,name)
+
+
+// var age=23;
+// console.log(age);
+
+// var age;  //declaration -> box created without values
+// age=23;   //initialization -> box me value de di
+// console.log(age);
+
+
+// let name = "nishan";
+// name="ram"; // updatipon allows hai
+
+// console.log(name);
+
+// let name="saini";  //re-declaration is not all 
+// console.log(name);
+
+
+// const aadharCard=9911218903;
+// // aadharCard =888899599;  //updation not allows;
+// console.log(aadharCard);
+// const aadharCard=445545;  //re-declaration not allows
+
+
+ // VARIABLE NAME IS MENING FULL HONA CHAHIYE;
+
+ let $prise=455;
+ let _crazy ="ndjd";
+ let age = 23;
+ console.log(typeof age);
+ console.log(typeof _crazy);
+
+ const doubleQuote="abhinan";
+ const singleQuote='hi my name is nishant';
+ const temlateLiteral=`hi my name`
+ console.log(doubleQuote);
+ console.log(singleQuote);
+ console.log(doubleQuote);
+ 
+
+ 
+
+
+ 
+
---
+++ lecture30 Operater conditional_js/index.js
@@ -0,0 +1,66 @@
+// Airtmatic Operater
+
+//  let num1 =2;
+//  let num2 =4;
+//  console.log(num1+num2)
+//  console.log(num1-num2)
+//  console.log(num1*num2)
+//  console.log(num1%num2)
+//  console.log(num1**num2)
+
+
+//increment and decrement
+// let num=45;
+// num++;
+
+// console.log(++num);
+// console.log(num);
+// console.log(++num);
+
+
+// let num=2;
+// num +=5;
+// console.log(num);
+// num -=5;
+// console.log(num);
+
+
+// let num = 2;
+
+// num += 5;
+// console.log(num);
+
+// num -= 5;
+// console.log(num);
+
+// num *= 5;
+// console.log(num);
+
+// num /= 5;
+// console.log(num);
+
+// num %= 5;
+// console.log(num);
+
+// num **= 5;
+// console.log(num);
+
+
+
+//Comparision operater 
+// const num1 = 3;
+// const num2 = 6;
+
+// console.log(3 > 6);   // false
+// console.log(3 < 6);   // true
+// console.log(3 <= 6);  // true
+// console.log(3 == 6);  // false
+// console.log(3 != 6);  // true
+
+// loose equality
+console.log("5" == 5);   // true
+
+// strict equality
+console.log("5" === 5);  // false
+
+
