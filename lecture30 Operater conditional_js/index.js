b73010a8b20d085a2b3be8e4671c6baeeb9eab2f
ABHINANDAN VERMA <abhinandanverma0152@gmail.com>
2026-08-24 22:40:32 +0530

    update file

--- lecture30 Operater conditional_js/index.js
+++ lecture30 Operater conditional_js/index.js
@@ -58,9 +58,127 @@
 // console.log(3 != 6);  // true
 
 // loose equality
-console.log("5" == 5);   // true
+// console.log("5" == 5);   // true
 
 // strict equality
-console.log("5" === 5);  // false
+// console.log("5" === 5);  // false
+
+
+// console.log(4>3 && 4<8);
+// console.log(true && false);
+// console.log(true && true);
+// console.log(true && false);
+
+
+// console.log(4<3 && 4<8);
+// console.log(true or false);
+// console.log(true && true);
+// console.log(true or false);
+
+
+// console.log(true || false);
+// console.log(!6665);
+// console.log(typeof NaN);
+
+// let day= "fri";
+
+// if (day === "mon") {
+//     console.log("1st day of the week");
+// } else if (day === "tue") {
+//     console.log("2nd day of the week");
+// } else if (day === "wed") {
+//     console.log("3rd day of the week");
+// } else if (day === "thu") {
+//     console.log("4th day of the week");
+// } else if (day === "fri") {
+//     console.log("5th day of the week");
+// } else if (day === "sat") {
+//     console.log("6th day of the week");
+// } else if (day === "sun") {
+//     console.log("7th day of the week");
+// }
+// else{
+//     console.log("wrong day");
+// }
+
+
+// const isLoggedIn = true;
+// const isSubscribed = false;
+
+// if (isLoggedIn) {
+//     if (isSubscribed) {
+//         console.log("You can access premium content.");
+//     } else {
+//         console.log("You don't have any premium plan to access this content.");
+//     }
+// } else {
+//     console.log("Please login");
+// }
+
+
+
+
+//switch case
+// const day = "fri";
+// switch (day) {
+//     case "mon":
+//         console.log("first day of the week");
+//         break;
+//     case "tue":
+//         console.log("2nd day of the week");
+//         break;
+//     case "wed":
+//         console.log("3nd day of the week");
+//         break;
+//     case "thu":
+//         console.log("4nd day of the week");
+//         break;
+//     case "fri":
+//         console.log("5nd day of the week");
+//         break;
+//     case "sat":
+//         console.log("6nd day of the week");
+//         break;
+//     case "sun":
+//         console.log("7nd day of the week");
+//         break;
+//     default:
+//         console.log("wrong day");
+// }
+
+
+
+const day = "fri";
+switch (day) {
+    case "mon":
+        console.log("first day of the week");
+
+    case "tue":
+        console.log("2nd day of the week");
+
+    case "wed":
+        console.log("3nd day of the week");
+
+    case "thu":
+        console.log("4nd day of the week");
+
+    case "fri":
+        console.log("5nd day of the week");
+
+    case "sat":
+        console.log("6nd day of the week");
+
+    case "sun":
+        console.log("7nd day of the week");
+
+    default:
+        console.log("wrong day");
+}
+
+
+
+
+
+
 
 
---
+++ lecture31 maths ,function ,loop/index.js
@@ -0,0 +1,64 @@
+// console.log(2+4);
+// for(let i = 1 ; i<=10 ; i++){
+//     console.log("hello");
+//     console.log(i);
+// }
+
+// let i=1;
+// while(i <= 5){
+//     console.log("ram");
+//     console.log(i);
+//     i++;
+// }
+
+
+// let i=1;
+// do{
+//     console.log(i);
+//     i++;
+// }while(i<5)
+
+
+//  for(let i = 1 ; i<=10 ; i++){
+//     if(i===3){
+//         break;
+//     }
+//     console.log(i);
+//  }
+
+
+
+// for(let i = 1 ; i<=10 ; i++){
+//     if(i===3){
+//         continue;
+//     }
+//     console.log(i);
+//  }
+
+
+
+// for(let i = 1 ; i<=10 ; i++){
+//     if(i===3 || i===6 || i===8){
+//         continue;
+//     }
+//     console.log(i);
+//  }
+
+
+// for(let i = 1 ; i<=10 ; i++){
+//     console.log("5 * " + i +"=" +(i*5));
+   
+//  }
+
+
+
+
+
+
+
+
+let i ;
+for (i=1; i<=10; i++ ){
+    console.log ("5 * " +i+"=" +(i*5));
+
+}
