fcf66e87d80d7e87579d6218ec0d2bb386ade309
ABHINANDAN VERMA <abhinandanverma0152@gmail.com>
2026-09-08 00:10:02 +0530

    update file

---
+++ lecture35 scope,and lexical environment/hosting.js
@@ -0,0 +1,29 @@
+// console.log(a)
+// // let a=6;
+// const a=6;  //error
+// var a=6;  //undifine
+
+
+
+// addNum()
+// function addNum(){
+//     console.log("hello");
+// }
+
+// console.log(addTwoNum)  //undifine
+// // addTwoNum()  //error
+// var addTwoNum = function(){
+//     console.log("function expression");
+// }
+
+// // addTwoNum();
+
+
+var a=5;
+function addNum(){
+    var a=6;
+    let b=7;
+    console.log(a);
+}
+addNum();
+
---
+++ lecture35 scope,and lexical environment/index.js
@@ -0,0 +1,56 @@
+
+// let username="abhinana";
+// console.log(username) 
+
+// // scope ->block
+// if(true){
+//     console.log("hello");  //block
+//     console.log("hiii");
+// }
+
+
+// {
+
+//     let city="ram"
+//     console.log(city);
+// }
+
+
+// function hello(){
+//     let state = "apple";
+//     console.log(state);
+// }
+
+
+// function hello() {
+//     let state = "Uttar Pradesh";
+//     console.log(state);
+
+//     var productName = "Iphone 1000 pro ulta max";
+//     console.log(productName);
+// }
+
+
+// function counter(){
+//     let count = 0
+//     count = count + 1
+//     console.log(count);
+    
+// }
+
+// counter()
+// counter()
+
+     //global declare
+//  let count = 0;
+// function counter(){
+//     count = count + 2
+//     console.log(count);
+// }
+
+// counter()
+// counter()
+
+
+
+//hosting
