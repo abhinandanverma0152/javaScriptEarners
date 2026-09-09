d05c896297dcdd60cfa293fcd02fa5b5ca9c06bc
ABHINANDAN VERMA <abhinandanverma0152@gmail.com>
2026-09-09 23:33:14 +0530

    update

--- lecture35 scope,and lexical environment/hosting.js
+++ lecture35 scope,and lexical environment/hosting.js
@@ -19,11 +19,65 @@
 // // addTwoNum();
 
 
-var a=5;
-function addNum(){
-    var a=6;
-    let b=7;
-    console.log(a);
-}
-addNum();
+// var a=5;
+// function addNum(){
+//     var a=6;
+//     let b=7;
+//     console.log(a);
+// }
+// addNum();
 
+// let x=6;
+//  var x=6;
+
+// function random(){
+//     console.log(x);
+//     // let x=3;
+//      var x=6;  //undefine
+// }
+// random();
+
+
+// let city="ballia";
+// function printcity(){
+//     let city="varansi"
+//     console.log(city)
+
+// }
+// printcity()
+
+
+// function outer() {
+
+
+// function inner() {
+
+// }
+
+// return inner();
+
+// }
+
+// const result = outer();
+
+// console.log(result);
+
+
+let username = "dhruv";
+
+// inner();
+
+function fun1() {
+
+    function fun2() {
+
+        function fun3() {
+
+            function fun4() {
+
+            }
+
+        }
+fun3();
+    }
+}
--- lecture35 scope,and lexical environment/index.js
+++ lecture35 scope,and lexical environment/index.js
@@ -54,3 +54,33 @@
 
 
 //hosting
+
+// var a=5;
+// let b=10;
+// console.log(a+b);
+
+
+
+var a = 5;
+let b = 10;
+
+console.log(a + b);
+
+function outer() {
+    let num1 = 10;
+    let num2 = 20;
+
+    function inner(){
+     let num1 = 50;
+    let num2 = 60;
+    return num1 + num2;
+    }
+const result=inner()+ num1+num2;
+ 
+    return result;
+}
+
+const result = outer();
+// const result2 = fun1();
+
+console.log(result);
